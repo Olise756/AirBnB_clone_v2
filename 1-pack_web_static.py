30ef6ded7885479de803e4b57533d4182ef01c0c
Olise756 <isiakponaolise@gmail.com>
2023-04-20 15:47:58 -0700

    tasks

---
+++ 1-pack_web_static.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+
+from fabric.api import local
+from time import strftime
+from datetime import date
+
+
+def do_pack():
+    """ This script generates archive the contents of web_static folder"""
+
+    fname = strftime("%Y%m%d%H%M%S")
+    try:
+        local("mkdir -p versions")
+        local("tar -czvf versions/web_static_{}.tgz web_static/"
+              .format(fname))
+
+        return "versions/web_static_{}.tgz".format(fname)
+
+    except Exception as e:
+        return None
